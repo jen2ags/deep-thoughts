9687559f95add886aeaba4a2ab6363266bd912b4
jennj <jennjenn93@gmail.com>
2022-06-27 16:00:40 -0500

    adding create, update, and delete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,13 +34,22 @@ type Query {
     thought(_id: ID!): Thought
     users: [User]
     user(username: String!): User
+    me: User
 
 }
 
  
 type Mutation {
-    login(email: String!, password: String!): User
-    assUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
 }
 `;
 
